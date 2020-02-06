8f4f188a8f03974abeb39e17a6ff56eaa8004960
Raja. Soundaramourty <rajasoun@cisco.com>
2020-02-06 22:20:06 +0530

    Refactore Code

--- control_center.bash
+++ control_center.bash
@@ -4,6 +4,28 @@ CONTROL_CENTER_COMPOSE_FILE="docker-compose.yml"
 OPTS="ANSIBLE_SCP_IF_SSH=TRUE ANSIBLE_CONFIG=/ansible/provision/ansible.cfg ANSIBLE_GATHERING=smart"
 ANSIBLE_OPTS="ansible_python_interpreter=/usr/bin/python3"
 
+function os(){
+  UNAME=$( command -v uname)
+  case $( "${UNAME}" | tr '[:upper:]' '[:lower:]') in
+    linux*)
+      printf 'linux\n'
+      ;;
+    darwin*)
+      printf 'mac\n'
+      ;;
+    msys*|cygwin*|mingw*)
+      # or possible 'bash on windows'
+      printf 'windows\n'
+      ;;
+    nt|win*)
+      printf 'windows\n'
+      ;;
+    *)
+      printf 'unknown\n'
+      ;;
+  esac
+}
+
 function verify_ansible_connection() {
   CMD="ansible-inventory  -i /inventory/hosts  --list "
   verify=$(CMD="$OPTS $CMD" MSYS_NO_PATHCONV=1 docker-compose -f "$CONTROL_CENTER_COMPOSE_FILE" run --rm service)
@@ -24,12 +46,22 @@ function configure_vm() {
 
 function control_center_shell() {
   CMD="/bin/bash"
-  CMD="$CMD" docker-compose -f "$CONTROL_CENTER_COMPOSE_FILE" run --rm service
+  CMD="$CMD" _docker_compose -f "$CONTROL_CENTER_COMPOSE_FILE" run --rm service
 }
 
 #ToDo: Assumes Existence of MonoRepo :-)
 function copy_keys(){
   rm -fr keys/* && cp ../multipass-dev-box/keys/multipass/id_rsa_bizapps* keys/
+}
+
+function _docker_compose() {
+  realdocker="$(which -a docker-compose | head -1)"
+  if [[ "$(os)" == "windows" ]]; then
+    export MSYS_NO_PATHCONV=1 MSYS2_ARG_CONV_EXCL="*"
+    printf "%s\0" "$@" > /tmp/args.txt
+    winpty bash -c "xargs -0a /tmp/args.txt '$realdocker' '$@'"
+  fi
+  $realdocker $@
 }
 
 copy_keys
